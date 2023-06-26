16b1630bd42c3f3af7f6ef5dbae4cf4577a64fa6
Anrii <andrejsalogub3@gmail.com>
2023-06-26 15:06:11 +0300

    added the ability to move list items

--- js/script.js
+++ js/script.js
@@ -15,15 +15,23 @@ const mostPopularAnimes = [
     "Demon Slayer",
 ];
 
-let dragStart;
+let dragStartIndex;
 
-const createList = () => {
-    mostPopularAnimes.forEach((anime, i) => {
-        const listItem = document.createElement("li");
-        listItem.classList.add("list-item");
-        listItem.setAttribute("data-index", i);
+createList();
 
-        listItem.innerHTML = `
+function createList() {
+    mostPopularAnimes
+        .map((anime) => ({ value: anime, sort: Math.random() }))
+        .sort((a, b) => {
+            return a.sort - b.sort;
+        })
+        .map((anime) => anime.value)
+        .forEach((anime, i) => {
+            const listItem = document.createElement("li");
+            listItem.classList.add("list-item");
+            listItem.setAttribute("data-index", i);
+
+            listItem.innerHTML = `
           <span class="list-item__number">${i + 1}</span>
           <div class="list-item__draggable" draggable="true">
             <p class="list-item__draggable-person-name">${anime}</p>
@@ -31,9 +39,59 @@ const createList = () => {
           </div>
         `;
 
-        listItems.push(listItem);
-        list.append(listItem);
-    });
-};
+            listItems.push(listItem);
+            list.append(listItem);
+        });
 
-createList();
+    addEventListeners();
+}
+
+function dragStart() {
+    dragStartIndex = +this.closest("li").getAttribute("data-index");
+    console.log(dragStartIndex);
+}
+
+function dragEnter() {
+    this.classList.add("over");
+}
+
+function dragLeave() {
+    this.classList.remove("over");
+}
+
+function dragDrop() {
+    this.classList.remove("over");
+    const dragEndIndex = this.getAttribute("data-index");
+    swapItems(dragStartIndex, dragEndIndex);
+}
+
+function swapItems(dragStartIndex, dragEndIndex) {
+    const firstItem = listItems[dragStartIndex].querySelector(
+        ".list-item__draggable"
+    );
+    const secondItem = listItems[dragEndIndex].querySelector(
+        ".list-item__draggable"
+    );
+
+    listItems[dragStartIndex].appendChild(secondItem);
+    listItems[dragEndIndex].appendChild(firstItem);
+}
+
+function addEventListeners() {
+    const draggableItems = document.querySelectorAll(".list-item");
+    const draggableZones = document.querySelectorAll(".list-item__draggable");
+
+    draggableZones.forEach((draggableZone) => {
+        draggableZone.addEventListener("dragstart", dragStart);
+    });
+
+    draggableItems.forEach((draggbleItem) => {
+        draggbleItem.addEventListener("dragover", (e) => {
+            e.preventDefault();
+        });
+
+        draggbleItem.addEventListener("dragenter", dragEnter);
+        draggbleItem.addEventListener("dragleave", dragLeave);
+        draggbleItem.addEventListener("drop", dragDrop);
+    });
+}
